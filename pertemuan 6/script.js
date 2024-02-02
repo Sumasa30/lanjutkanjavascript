ac9b8fb9297bc38bbda1269d3abea64407600355
Sumasa30 <komangberlian03@gmail.com>
2024-02-02 16:21:20 +0800

    java script

---
+++ pertemuan 6/script.js
@@ -0,0 +1,53 @@
+// type data 
+// type data primitif : 
+// - String
+// - integer 
+// - floak
+// - char
+
+// let nama ="berlian"
+// console.log("hello world")
+// console.log(nama)
+// console.log("210030417");
+// console.log("BD213");
+
+// console.log("type data primitif"); 
+// console.log("String");
+// console.log("integer");
+// console.log("floak");
+// console.log("char");
+
+// console.log("contoh string");
+// console.log("ini adalah type data string");
+// console.log(typeof("ini adalah type data string"));
+// console.log("contoh integer");
+// console.log(12345);
+// console.log(typeof(12345));
+// console.log(typeof("12345")); 
+// console.log("contoh floak");
+// console.log(2.5);
+// console.log(typeof(2.5));
+// console.log("contoh char");
+// console.log('a');
+// console.log(typeof('a'));
+
+let nama ="I KOMANG PUTRA BERLIAN SUMASA"
+let umur =20
+let tanggal_lahir ="20 april 2003"
+let about_me ="nama saya adalah I KOMANG PUTRA BERLIAN SUMASA biasa di panggil berlian,"
+let about_me1 ="saya kuliah dibidang sistem informasi dan saya sekarang baru terjun di dunia it"
+let about_me2 ="basic saya adalah pembisnis, saya senang melakukan bisnis-bisnis baru dan mencari peluang bisnis"
+let about_me3 ="disinilah alasan saya terjun di dunia IT untuk menyambungkan bisnis saya ke teknologi yang semakin berkembang"
+let about_me4 ="karena dari pemasaran teknologi bisnis kita bisa diakses seluruh dunia,"
+let about_me5 ="saya mempunyai moto yaitu : JANGAN PERNAH MENYERAH SEBELUM MENCAPAI KESUKSESAN"
+
+console.log(nama);
+console.log(umur);
+console.log(tanggal_lahir);
+console.log(about_me);
+console.log(about_me1);
+console.log(about_me2);
+console.log(about_me3);
+console.log(about_me4);
+console.log(about_me5);
+
---
+++ pertemuan 7/home.js
@@ -0,0 +1,52 @@
+console.log("pertemuan 7");
+// let angka_1=10
+// let angka_2=11
+// let hasil_penjumlahan=angka_1+angka_2
+// let hasil_pengurangan=angka_1-angka_2
+// let hasil_pembagian=angka_1/angka_2
+// let hasil_perkalian=angka_1*angka_2
+// console.log(hasil_penjumlahan);
+// console.log(hasil_pengurangan);
+// console.log(hasil_pembagian);
+// console.log(hasil_perkalian);
+
+// let angka1=10
+// let angka2=11
+// let angka3=50
+// let hasilpenjumlahan=angka1+angka2-angka3
+// let hasilpengurangan=angka1-angka2+angka3
+// let hasilpembagian=angka1/angka2*angka3
+// let hasilperkalian=angka1*angka2+angka3
+// console.log(hasilpenjumlahan);
+// console.log(hasilpengurangan);
+// console.log(hasilpembagian);
+// console.log(hasilperkalian);
+
+// let nama_depan="I komang putra"
+// let nama_belakang=" berlian sumasa"
+// let hasil=nama_depan+nama_belakang
+// console.log(hasil.toLowerCase());
+
+// let namadepan="restaurant"
+// let namabelakang=" kampoeng kepiting"
+// let hasill=namadepan+namabelakang
+// console.log(hasill.toUpperCase());
+
+let depan="==========pertemuan 7=========="
+let judul="kalkulator"
+let isi1=2
+let isi2=5
+let jumlah=isi1+isi2
+let kurang=isi1-isi2
+let kali=isi1*isi2
+let bagi=isi1/isi2
+let nama_belakang="==========pertemuan 7=========="
+console.log(depan);
+console.log(judul);
+console.log("angka 1 :" , isi1);
+console.log("angka 2 :" , isi2);
+console.log("hasil penjumlahan =" , jumlah);
+console.log("hasil pengurangan =" , kurang);
+console.log("hasil perkalian =" , kali);
+console.log("hasil pembagian =" , bagi);
+console.log(nama_belakang);
