a8e14459b012744e86b797843100aa2fc118bcc1
Sumasa30 <komangberlian03@gmail.com>
2024-02-20 15:16:43 +0800

    firt commit

---
+++ pertemuan 10/berlian.js
@@ -0,0 +1,35 @@
+function clickpenjumlahan() {
+    let valuenama1 = document.getElementById("inputnama1")
+    let valuenama2 = document.getElementById("inputnama2")
+    let angka1=parseInt(valuenama1.value)
+    let angka2=parseInt(valuenama2.value)
+    let hasil=angka1 + angka2
+    alert(hasil)
+}
+
+function clickpengurangan() {
+    let valuenama1 = document.getElementById("inputnama1")
+    let valuenama2 = document.getElementById("inputnama2")
+    let angka1=parseInt(valuenama1.value)
+    let angka2=parseInt(valuenama2.value)
+    let hasil=angka1 - angka2
+    alert(hasil)
+}
+
+function clickperkalian() {
+    let valuenama1 = document.getElementById("inputnama1")
+    let valuenama2 = document.getElementById("inputnama2")
+    let angka1=parseInt(valuenama1.value)
+    let angka2=parseInt(valuenama2.value)
+    let hasil=angka1 * angka2
+    alert(hasil)
+}
+
+function clickpembagian() {
+    let valuenama1 = document.getElementById("inputnama1")
+    let valuenama2 = document.getElementById("inputnama2")
+    let angka1=parseInt(valuenama1.value)
+    let angka2=parseInt(valuenama2.value)
+    let hasil=angka1 / angka2
+    alert(hasil)
+}
